4b6b633c5dcbaafaa7b9c3f9d2262523a817a6ce
CaparicaLeo <leo_caparica@icloud.com>
2024-05-15 19:24:52 -0300

    Creating routes

---
+++ app/Http/Controllers/SignUpController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SignUpController extends Controller
+{
+    public function signup(Request $request) : string{
+        dd($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\SignUpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/signin', function () {
+    return view('signin');
 });
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+Route::post('/signup', [SignUpController::class, 'signup'])->name('signup');
+
